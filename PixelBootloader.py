0ca76163d9ad3f6f5a3b3ba27ee6e08e3198a7df
0xAbby <128301871+0xAbby@users.noreply.github.com>
2025-02-07 16:26:26 +0200

    Update PixelBootloader.py
    support for loading abl without function strings

--- PixelBootloader.py
+++ PixelBootloader.py
@@ -4,7 +4,7 @@
 #		for Google Pixel Phones, tested against abl binaries from:
 #		-	Pixel 6 / 6a / 6 pro
 #		-	Pixel 7 / 7 pro
-#		- Pixel 8
+#		-       Pixel 8
 #
 #		The loader was tested in IDA Pro version 7.6 - 8.3
 #   Author:
@@ -21,6 +21,7 @@ import ida_ua
 import ida_typeinf
 import os
 import struct
+from time import sleep
 
 # Read function table and set function names.
 def resolve_func_table(offset):
@@ -46,36 +47,44 @@ def resolve_func_table(offset):
 	# For each function in the table, get its offset,
 	# Tell IDA to analyze/make it code, then set func name.
 
+
+	output_csv = open("func_csv", "w")
 	while address_table_start < address_table_end:
-		# get function offset
+    	# get function offset
 		func_offset = ida_bytes.get_bytes(address_table_start, 8)
 
-		# read function address
+    	# read function address
 		func_offset_bytes = int.from_bytes(func_offset, "little")
 
-		# read offset value of  "function name"
+    	# read offset value of  "function name"
 		string_offset = ida_bytes.get_bytes(address_table_start+12, 4)
 
-		# find function name (names offset + names table)
+    	# find function name (names offset + names table)
 		func_name_addr = int.from_bytes(string_offset, "little") + address_table_end
 		idc.create_strlit(func_name_addr, idc.BADADDR)
 		func_name_len = ida_bytes.get_max_strlit_length(func_name_addr, idc.STRTYPE_C, ida_bytes.ALOPT_IGNHEADS)
 		funct_str = ida_bytes.get_strlit_contents(func_name_addr, func_name_len, idc.STRTYPE_C).decode()
+		csv_entry = "" + '\"' + funct_str + '\", \"' + hex(func_name_addr) + "\"\n"
+		output_csv.write(csv_entry)
+        
 		#if funct_str == "pixel_loader_entry":
-		#	ida_entry.add_entry(func_offset_bytes, func_offset_bytes, funct_str, True, 0)
+        #	ida_entry.add_entry(func_offset_bytes, func_offset_bytes, funct_str, True, 0)
 
-		# analyze/set function to code, set name, mark offset / value in table.
+        # analyze/set function to code, set name, mark offset / value in table.
 		ida_ua.create_insn(func_offset_bytes)
 		ida_name.set_name(func_offset_bytes, funct_str, idaapi.SN_NOWARN | idaapi.SN_NOCHECK | ida_name.SN_FORCE)
 		idc.op_plain_offset(address_table_start, 0, 0)
 		ida_bytes.create_dword(address_table_start+8, 4) 
 		ida_bytes.create_dword(address_table_start+12, 4) 
-		
-		# Set data structure for each function entry
+        
+        # Set data structure for each function entry
 		idc.apply_type(address_table_start, _type, 0)
-		
-		# next entry in functions table (entry is 16 byte long)
+        
+        # next entry in functions table (entry is 16 byte long)
 		address_table_start += 16
+	output_csv.close()
+
+  
 	
 
 def get_file_size(blob):
@@ -109,9 +118,16 @@ def create_segment(start, end, bitness, name, segType):
 	seg = idaapi.segment_t()
 	seg.start_ea = start
 	seg.end_ea =  end
+	if segType == "CODE":
+		seg.perm = idaapi.SEGPERM_READ | idaapi.SEGPERM_EXEC  | idaapi.SEGPERM_WRITE
+	else:
+		seg.perm = idaapi.SEGPERM_READ | idaapi.SEGPERM_WRITE
+	seg.is_64bit = True
+#	seg.align = 3
 	seg.bitness = bitness
 	
 	idaapi.add_segm_ex(seg, name, segType, 0)
+
 
 
 # given a start/end address, search for 'bytes_str'
@@ -136,7 +152,7 @@ def search_bytes(start_ea, end_ea, bytes_str):
  
 # search for and return offset of function table, and offset where code segment ends
 def find_func_table(filesize_):
-	data_search_offset = filesize_ - int(filesize_ / 14) 
+	data_search_offset = filesize_ - int(filesize_ * 0.2) 
 	func_table_offset = search_bytes(data_search_offset, filesize_, "FFFF0000F8800000")
 
 	if func_table_offset:
@@ -185,6 +201,49 @@ def create_structs():
 	for struct in def_structs:
 		ida_typeinf.idc_parse_types(struct, 0)
 
+def find_sig(sig, ea_start, ea_end):
+
+    while ea_start != idaapi.BADADDR:
+        
+        ea_start = idaapi.find_binary(ea_start, ea_end, sig, 16, idaapi.SEARCH_DOWN)
+        insn = idaapi.insn_t()
+        if ea_start != idaapi.BADADDR:
+            if idaapi.decode_insn(insn, ea_start):
+                if (insn.itype in [idaapi.ARM_stp, idaapi.ARM_mov, idaapi.ARM_sub]):
+                    ida_ua.create_insn(ea_start)
+                    idaapi.add_func(ea_start, idaapi.BADADDR)
+            ea_start += 4
+
+def find_code_by_prologue(ea_start, ea_end):
+    prologue = {"fd 7b 01 ?",
+                "fd 7b b8 ?",
+                "fd 7b b9 ?",
+                "fd 7b bb ?",
+                "fd 7b ba ?",
+                "fd 7b be ?",
+                "fd 7b bc ?",
+                "fd 7b bd ?",
+                "fd 7b bf ?",
+                "ff c3 02 ?",
+                "ff c3 00 ?",
+                "ff 03 03 ?",
+                "ff 03 01 ?",
+                "ff 43 01 ?",
+                "ff 83 02 ?",
+                "ff 83 01 ?" }
+
+
+    for sig in prologue:
+        find_sig(sig, ea_start, ea_end)
+
+def find_possible_code_end(ea, maxea):
+    sig = "00 " * 0x230
+    while ea != idaapi.BADADDR:
+        ea = idaapi.find_binary(ea, maxea, sig, 16, idaapi.SEARCH_DOWN)
+        if ea != idaapi.BADADDR:
+            #print("Found at ", hex(ea))
+            return ea
+    return 0
 
 def resolve_fastboot_table():
 	pass
@@ -201,26 +260,45 @@ def load_file(binaryBlob, neflags, format):
 	create_segment(0, filesize_, 2, "BLOB", "DATA")
 	binaryBlob.seek(0)
 	binaryBlob.file2base(0, 0, filesize_, False)
+    
 	
 	# find function table / and end of code / offset where code segment ends 
 	func_table_offset, end_of_code_segment = find_func_table(filesize_) 
 	
 	if not func_table_offset:
 		print("# PixelAblLoader: failed, existing")
-		return -1
+		print("# continuing without function table")
+        
+		print("# Searching for end of CODE section")
+		possible_code_end = find_possible_code_end( int(filesize_*0.4), int(filesize_*0.57))
+        
+		if possible_code_end != 0:
+			create_segment(0, (possible_code_end), 2, "ABL_CODE", "CODE")
+			create_segment((possible_code_end), filesize_, 2, "ABL_DATA", "DATA")
+			ida_segment.rebase_program(base_addr, 0 )
+		else:
+			print("# No end of CODE section, treat segment as code")
+			create_segment(0, filesize_, 2, "ABL", "CODE")
+			ida_segment.rebase_program(base_addr, 0)
+        
+		ida_ua.create_insn(base_addr)
+		idaapi.add_func(base_addr, idaapi.BADADDR)    
+		find_code_by_prologue(base_addr, base_addr+filesize_)
+
+	elif func_table_offset:
+		# adjust code and data segments / update base address
+        
+		create_segment(0, (end_of_code_segment - base_addr), 2, "ABL_CODE", "CODE")
+		create_segment((end_of_code_segment - base_addr), filesize_, 2, "ABL_DATA", "DATA")
+		ida_segment.rebase_program(base_addr, 0 )	
+        
+		# create releavant data structures from C-style definitions
+		create_structs()
+        
+		# set function' names / and tell ida pro to make each function into code 
+		resolve_func_table(func_table_offset + base_addr)
+        
+		# find and set fastboot commands and var table structs
+		resolve_fastboot_table()
 		
-	# adjust code and data segments / update base address
-	create_segment(0, (end_of_code_segment - base_addr), 2, "ABL_CODE", "CODE")
-	create_segment((end_of_code_segment - base_addr), filesize_, 2, "ABL_DATA", "DATA")
-	ida_segment.rebase_program(base_addr, 0 )	
-	
-	# create releavant data structures from C-style definitions
-	create_structs()
-	
-	# set function' names / and tell ida pro to make each function into code 
-	resolve_func_table(func_table_offset + base_addr)
-	
-	# find and set fastboot commands and var table structs
-	resolve_fastboot_table()
-	
 	return 1
